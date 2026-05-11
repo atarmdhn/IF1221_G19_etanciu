ef83eb06ce89946de36e94250fe12815a15ab73c
Naufal Hasbialhaq <naufal.hasbialhaq18@gmail.com>
2026-05-11 15:33:41 +0700

    feat : fakta warna, number, kartu warna biru

---
+++ src/fact.pl
@@ -0,0 +1,22 @@
+% Rule Kartu Biru
+kartu(biru, 0). kartu(biru, 1). kartu(biru, 2). kartu(biru, 3). kartu(biru, 4).
+kartu(biru, 5). kartu(biru, 6). kartu(biru, 7). kartu(biru, 8). kartu(biru, 9).
+
+kartu(biru, skip). kartu(biru, reverse). kartu(biru, draw_two).
+
+number(0).
+number(1). 
+number(2). 
+number(3).
+number(4).
+number(5).
+number(6).
+number(7).
+number(8).
+number(9).
+
+warna(biru).
+warna(merah).
+warna(hitam).
+warna(hijau).
+warna(kuning).
