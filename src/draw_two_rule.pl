52fe0be73b4ace9755c8c1687e102d6184ad2bdd
kururi <fakhriyan.rizki@gmail.com>
2026-05-12 10:37:50 +0700

    fix: samain variabel dan samain fungsi sama yang lain

--- src/draw_two_rule.pl
+++ src/draw_two_rule.pl
@@ -1,16 +1,9 @@
-% cek apakah sebelumnya draw_two atau bukan
-cekValid(Warna, draw_two) :- 
-    top_pile(WarnaBef, NilaiBef), 
-    NilaiBef \= draw_two,
-    Warna == WarnaBef
-
 % rule drawTwo
 drawTwo(kartu(Warna, draw_two)) :- 
-    cekValid(Warna, draw_two),
     giliran(Pemain),
 
-    retract(top_pile(_, _)),            % Ngubbah top_pile sebelumnya jadi kartu draw_two yang dimainkan
-    asserta(top_pile(Warna, draw_two)),
+    retract(kartu(_WarnaMeja, _)),            % Ngubah top_pile sebelumnya jadi kartu draw_two yang dimainkan
+    asserta(kartu(Warna, draw_two)),
 
     pindah_giliran(NextPlayer), % ubah giliran
 
