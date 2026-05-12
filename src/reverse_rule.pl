a6fb1769c6de6ce628eb876b4fd3cdcdc925879b
Naufal Hasbialhaq <naufal.hasbialhaq18@gmail.com>
2026-05-12 10:08:40 +0700

    fix : menyesuaikan predikat list player

--- src/reverse_rule.pl
+++ src/reverse_rule.pl
@@ -1,5 +1,5 @@
 reverseRule(kartu(_, skip)):-
-    urutanPemain(UrutanLama), % urutanPemain belum ada
+    getAllPlayers(UrutanLama), % urutanPemain belum ada
     balikUrutan(UrutanLama, UrutanBaru),
 
     retract(UrutanLama),
