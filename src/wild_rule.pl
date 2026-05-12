98968052528815857c81ef860a10abc68789a639
Muhammad Atallah Ramadhan <atarmdhn@Muhammads-MacBook-Air.local>
2026-05-12 09:39:07 +0700

    revisi rule wild

--- src/wild_rule.pl
+++ src/wild_rule.pl
@@ -1,7 +1,7 @@
-wildRule(kartu(_,wild), _KartuMeja, WarnaBaru) :-
+wildRule(kartu(hitam,wild), _KartuMeja, WarnaBaru) :-
     warna(WarnaBaru).
 
-wildDrawFour(kartu(_,wild), _KartuMeja, WarnaBaru) :-
+wildDrawFour(kartu(hitam,drawFour), _KartuMeja, WarnaBaru) :-
     tambah_kartu(NextPlayer, ambilKartu(Kartu1)),
     tambah_kartu(NextPlayer, ambilKartu(Kartu2)),
     tambah_kartu(NextPlayer, ambilKartu(Kartu3)),
