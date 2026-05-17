50a4f19918725a0d6d101d5af8c07bcb438002f1
Vinsju <v.juansetiady@gmail.com>
2026-05-17 12:49:32 +0700

    refactor : all info stored in dynamic database

--- src/startGame.pl
+++ src/startGame.pl
@@ -1,24 +1,43 @@
-:- dynamic(player/1).
+:- dynamic(player/2). % menyimpan tangan player(Nama,ListKartu)
+:- dynamic(playerOrder/1). % menyimpan urutan giliran
+:- dynamic(currentPlayer/1). % menyimpan pemain yang mendapat giliran
+:- dynamic(gameDirection/1). % menyimpan alur permainan (normal/reverse).
+:- dynamic(topCard/1). % menyimpan info kartu teratas di meja 
+
+:- dynamic(tempPlayer/1). % menyimpan input nama pemain 
+
 :- include('importantFunctions.pl').
 :- include('fact.pl').
 
 startGame :-
-    retractall(player(_)),
+    retractall(player(_,_)),
+    retractall(playerOrder(_)),
+    retractall(currentPlayer(_)),
+    retractall(gameDirection(_)),
+    retractall(topCard(_)),
+    retractall(tempPlayer(_)),
+
     askPlayerCount(Count),
     askPlayerNames(1, Count),
     getAllPlayers(Players),
+
     randomList(Players,RandomPlayers),
     write('Urutan pemain: '), printList(RandomPlayers), nl,
+    assertz(playerOrder(RandomPlayers)),
+
+    [First|_] = RandomPlayers,
+    assertz(currentPlayer(First)),
+    assertz(gameDirection(normal)),
     write('Setiap pemain mendapatkan 7 kartu acak.'), nl,
 
     myFindall(kartu(Warna,Jenis), kartu(Warna,Jenis), ListKartu),
     randomList(ListKartu,DeckAcak),
-    bagiKartu(RandomPlayers,DeckAcak,ListPemainDanKartu,SisaDeckSetelahDibagi),
+    bagiKartu(RandomPlayers,DeckAcak,SisaDeckSetelahDibagi),
     getTopCard(SisaDeckSetelahDibagi,TopCard,FinalDeck),
+    assertz(topCard(TopCard)),
+
     kartu(Warna,Jenis) = TopCard,
     format('Kartu discard top: ~w - ~w', [Warna,Jenis]), nl, 
-    
-    [First|_] = RandomPlayers,
     format('Giliran ~w.', [First]), nl.
 
 /* Meminta input jumlah pemain */
@@ -28,7 +47,7 @@ askPlayerCount(Count) :-
     integer(In),
     In >= 2,
     In =< 4, 
-    Count = In.
+    Count = In, !.
 
 askPlayerCount(Count) :-
     write('Mohon masukkan angka antara 2 - 4.'), nl,
@@ -44,20 +63,20 @@ askPlayerNames(Count, Total) :-
     nameInputProcess(Count, Total, Name).
 
 nameInputProcess(Count, Total, Name) :-
-    \+ player(Name),
-    assertz(player(Name)),
+    \+ tempPlayer(Name),
+    assertz(tempPlayer(Name)),
     Next is Count + 1,
     askPlayerNames(Next, Total).
 
 nameInputProcess(Count, Total, Name) :-
-    player(Name),
+    tempPlayer(Name),
     write('Nama sudah digunakan. Masukkan nama lain: '),
     read(NewName),
     nameInputProcess(Count, Total, NewName).
 
 /* Memasukkan semua pemain ke list Players */
 getAllPlayers(Players) :-
-    findall(P, player(P), Players).
+    myFindall(P, tempPlayer(P), Players).
 
 /* Print pemain di dalam list Players */
 printList([H]) :- 
@@ -71,7 +90,7 @@ printList([H|T]) :-
 /* Validasi top card valid (bukan utiity card)*/
 
 getTopCard([CurrentCard|SisaDeck],TopCard,FinalDeck) :-
-    CurrentCard = kartu(Warna,Jenis),
+    CurrentCard = kartu(_,Jenis),
     \+ number(Jenis),!,
     myAppend(SisaDeck,CurrentCard,DeckBaru),
     getTopCard(DeckBaru,TopCard,FinalDeck).
@@ -81,10 +100,11 @@ getTopCard([CurrentCard|SisaDeck],CurrentCard,SisaDeck) :- !.
 /* Bagi kartu ke pemain */
 ambil7([A,B,C,D,E,F,G|SisaDeck],[A,B,C,D,E,F,G], SisaDeck).
 
-bagiKartu([], Deck, [], Deck).
-bagiKartu([Nama|SisaNama], DeckAwal, [player(Nama,Kartu)|SisaPlayer], DeckAkhir) :-
+bagiKartu([], Deck, Deck).
+bagiKartu([Nama|SisaNama], DeckAwal, DeckAkhir) :-
     ambil7(DeckAwal, Kartu, SisaDeck),
-    bagiKartu(SisaNama,SisaDeck,SisaPlayer,DeckAkhir).
-/* Hasil : [player(ata,[kartu(kuning,skip),kartu(biru,5),...)]), 
-            player(kuri,[kartu(merah,4),...]),
-            ...] */
+    assertz(player(Nama,Kartu)),
+    bagiKartu(SisaNama,SisaDeck,DeckAkhir).
+/* Hasil : player(ata,[kartu(kuning,skip),kartu(biru,5),...)]), 
+        player(kuri,[kartu(merah,4),...]),
+        ... */
