4e6b8fa138aebd0bb0ce505e4de7385de93ff218
Vinsju <v.juansetiady@gmail.com>
2026-05-17 15:27:40 +0700

    feat : mainkanKartu dan rulePemain

--- src/mainkanKartu.pl
+++ src/mainkanKartu.pl
@@ -1,4 +1,68 @@
-:- include('findall.pl').
+:- include('importantFunctions.pl').
+:- include('rulePemain.pl').
 
-randomTujuhKartu :-
-    myFindall(kartu(Warna, Jenis), kartu(Warna, Jenis), ListKartu),
+mainkanKartu(NomorUrut) :-
+    currentPlayer(PemainAktif),
+    topCard(CurrentTop),
+
+    retract(player(PemainAktif,TanganLama)),
+    Indeks is NomorUrut - 1,
+    eksekusiKartu(PemainAktif, TanganLama, Indeks, CurrentTop).
+
+/* Kondisi 1 : Nomor urut tidak valid */
+eksekusiKartu(PemainAktif, TanganLama, Indeks, _) :-
+    \+ getAndRemove(Indeks,TanganLama,_,_), !,
+    assertz(player(PemainAktif,TanganLama)),
+    write("Nomor urut invalid!"), fail.
+
+/* Kondisi 2 : Kartu yang dipilih tidak sesuai dengan kartu meja */
+eksekusiKartu(PemainAktif,TanganLama,Indeks,CurrentTop) :-
+    getAndRemove(Indeks,TanganLama,KartuPilihan,_),
+    \+ isKartuValid(KartuPilihan,CurrentTop), !,
+    assertz(player(PemainAktif,TanganLama)),
+    write("Kartu tidak sesuai dengan kartu di meja!"), fail.
+
+/* Kondisi 3 : Kartu yang dipilih valid */
+eksekusiKartu(PemainAktif, TanganLama, Indeks, CurrentTop) :-
+    getAndRemove(Indeks, TanganLama, KartuPilihan, TanganBaru),
+    isKartuValid(KartuPilihan,CurrentTop), !,
+    retract(topCard(CurrentTop)),
+    assertz(topCard(KartuPilihan)),
+    assertz(player(PemainAktif,TanganBaru)),
+    kartu(Warna,Jenis) = KartuPIlihan,
+    format('~w memainkan kartu : ~w - ~w', [PemainAktif,Warna,Jenis]).
+    prosesEfekdanTurn(KartuPilihan).
+
+/* Memeriksa apakah kartu valid */
+isKartuValid(kartu(W,_),kartu(W,_)) :- !.
+isKartuValid(kartu(_,J),kartu(_,J)) :- !.
+
+/* Proses efek dan turn */
+
+/* Normal turn */
+prosesEfekdanTurn(_) :-
+    getNextPlayer(PemainNext),
+    retract(currentPlayer(_)),
+    assertz(currentPlayer(PemainNext)),
+    format('Giliran ~w', [PemainNext]).
+
+/* Skip */
+prosesEfekdanTurn(kartu(_,skip)) :-
+    getNextPlayer(PemainNext),
+    retract(currentPlayer(_)),
+    assertz(currentPlayer(PemainNext)),
+    getNextPlayer(PemainSetelahnya),
+    retract(currentPlayer(_)),
+    assertz(currentPlayer(PemainSetelahnya)),
+    format('Giliran ~w', [PemainSetelahnya]).
+
+/* Reverse */
+prosesEfekdanTurn(kartu(_,reverse)) :-
+    retract(playerOrder(UrutanPemain)),
+    ubahArahPermainan,
+    getNextPlayer(PemainNext),
+    retract(currentPlayer(_)), assertz(currentPlayer(PemainNext)),
+    format('Giliran ~w', [PemainNext]).
+    
+
+
---
+++ src/rulePemain.pl
@@ -0,0 +1,45 @@
+getNextPlayer(Next) :-
+    gameDirection(normal), !,
+    currentPlayer(Current),
+    playerOrder(Order),
+    findNext(Current,Order,Next).
+
+getNextPlayer(Next) :-
+    gameDirection(reverse), !,
+    currentPlayer(Current),
+    playerOrder(Order),
+    findPrevious(Current,Order,Next).
+
+/* Mencari player selanjutnya jika alur normal */
+findNext(Current,[Current,Next|_],Next) :- !.
+
+findNext(Current,[_|T],Next) :-
+    findNext(Current,T,Next).
+
+findNext(Current,[Current],Next) :-
+    playerOrder([Next|_]).
+
+/* Mencari player selanjutnya jika alur reverse */
+findPrevious(Current,[Next,Current|_],Next) :- !.
+
+findPrevious(Current,[_|T],Next) :- 
+    findPrevious(Current,T,Next).
+
+findPrevious(Current,[Current|_],Next) :-
+    playerOrder(Order),
+    ambilElemenTerakhir(Order,Next), !.
+
+ubahArahPermainan :-
+    retract(gameDirection(normal)),
+    assertz(gameDirection(reverse)).
+    write('Arah giliran pemain berubah menjadi REVERSE.').
+
+ubahArahPermainan :-
+    retract(gameDirection(reverse)),
+    assertz(gameDirection(normal)),
+    write('Arah giliran pemain berubah menjadi NORMAL.').
+
+ambilElemenTerakhir([X], X) :- !.
+ambilElemenTerakhir([_|T],X) :-
+    ambilElemenTerakhir(T,X).
+
