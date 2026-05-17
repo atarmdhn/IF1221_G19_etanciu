cb4c48b62e8036a9bd16be8afaa189d6900f82c7
kururi <fakhriyan.rizki@gmail.com>
2026-05-17 12:49:03 +0700

    feat: nambahin feat mySort

--- src/importantFunctions.pl
+++ src/importantFunctions.pl
@@ -54,3 +54,34 @@ getLength([], 0).
 getLength([_|Tail], Length) :-
     getLength(Tail, TailLength),
     Length is TailLength + 1.
+
+/* Sort */
+mySort([], []).
+mySort([H|T], Sorted) :-
+    mySort(T, SortedTail),
+    insert_stats(H, SortedTail, Sorted).
+
+% insert_stats/3: Memasukkan elemen ke posisi yang tepat
+insert_stats(X, [], [X]).
+
+% Jika X harus diletakkan SEBELUM Head, maka jadikan X sebagai Head baru
+insert_stats(stats(P1, K1, U1, N1), [stats(P2, K2, U2, N2)|T], [stats(P1, K1, U1, N1), stats(P2, K2, U2, N2)|T]) :-
+    bandingkan_stats(stats(P1, K1, U1, N1), stats(P2, K2, U2, N2)), !.
+
+% Jika tidak, terus cari posisinya di bagian Tail/Sisanya
+insert_stats(X, [H|T], [H|Rest]) :-
+    insert_stats(X, T, Rest).
+
+/*Buat nyari poin*/
+
+% Kondisi 1: Poin lebih kecil menempati peringkat lebih tinggi
+bandingkan_stats(stats(P1, _, _, _), stats(P2, _, _, _)) :- 
+    P1 < P2.
+
+% Kondisi 2: Jika poin sama, jumlah kartu lebih sedikit diutamakan
+bandingkan_stats(stats(P, K1, _, _), stats(P, K2, _, _)) :- 
+    K1 < K2.
+
+% Kondisi 3: Jika poin dan kartu sama, urutan bermain awal menentukan
+bandingkan_stats(stats(P, K, U1, _), stats(P, K, U2, _)) :- 
+    U1 < U2.
