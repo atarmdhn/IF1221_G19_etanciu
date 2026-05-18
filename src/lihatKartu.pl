ec1a259bfed6caca4fa0482846f1eae8a5469fc9
kururi <fakhriyan.rizki@gmail.com>
2026-05-18 15:52:53 +0700

    fix: samain variabel giliran

--- src/lihatKartu.pl
+++ src/lihatKartu.pl
@@ -1,12 +1,12 @@
-lihatKartu :-
-    currentPlayer(Pemain),
-    player(Pemain, ListKartu),
-    format('Berikut kartu yang anda miliki.~n'),
-    cetakList(ListKartu, 1).
+    lihatKartu :-
+        currentPlayer(Pemain),
+        player(Pemain, ListKartu),
+        write('Berikut kartu yang anda miliki.'), nl,
+        cetakList(ListKartu, 1).
 
-cetakList([], _).
+    cetakList([], _).
 
-cetakList([kartu(Warna, Jenis) | Sisa], IndexKartu):-
-    format('~w. ~w-~w~n',[IndexKartu, Warna, Jenis]),
-    NextIndex is IndexKartu + 1,
-    cetakList(Sisa, NextIndex).
+    cetakList([kartu(Warna, Jenis) | Sisa], IndexKartu):-
+        format('~w. ~w-~w~n',[IndexKartu, Warna, Jenis]),
+        NextIndex is IndexKartu + 1,
+        cetakList(Sisa, NextIndex).
