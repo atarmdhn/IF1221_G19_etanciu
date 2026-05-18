f080aaa106869b4dae8caad1d6c57fd47d809c4b
kururi <fakhriyan.rizki@gmail.com>
2026-05-18 17:03:27 +0700

    fix : fixing error in CekInfo

--- src/cekInfo.pl
+++ src/cekInfo.pl
@@ -1,15 +1,17 @@
 cekInfo :-
     currentPlayer(Pemain),
-    format('Kartu discard top: ~w-~w~n', [WarnaMeja, Jenis]),
-    format('Urutan pemain: '), printList(Urutan), nl,
-    cetakInfoPemain(Pemain, 1).
+    topCard(kartu(WarnaMeja, JenisMeja)),
+    playerOrder(Urutan),
+    format('Kartu discard top: ~w-~w~n', [WarnaMeja, JenisMeja]),
+    write('Urutan pemain: '), printList(Urutan), write('.'), nl, nl,
+    cetakInfoPemain(Urutan, 1).
 
 cetakInfoPemain([], _).
 cetakInfoPemain([Pemain | SisaPemain], IndexPemain):-
     format('Nama pemain ~w: ~w~n', [IndexPemain, Pemain]),
-    kartuPemain(Pemain, ListKartu),
+    player(Pemain, ListKartu),
     length(ListKartu, BanyakKartu),
-    format('Jumlah kartu: ~w', BanyakKartu),
+    format('Jumlah kartu: ~w~n~n', [BanyakKartu]),
     NextIndex is IndexPemain + 1,
     cetakInfoPemain(SisaPemain, NextIndex).
 
