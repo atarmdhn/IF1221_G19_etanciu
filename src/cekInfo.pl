dae6f610626d2e9fc66d9876320a3b0e9d996e62
kururi <fakhriyan.rizki@gmail.com>
2026-05-19 10:39:27 +0700

    fix: bug urutan pemain ketika reverse

--- src/cekInfo.pl
+++ src/cekInfo.pl
@@ -2,9 +2,18 @@ cekInfo :-
     currentPlayer(Pemain),
     topCard(kartu(WarnaMeja, JenisMeja)),
     playerOrder(Urutan),
+    gameDirection(ArahMain)
     format('Kartu discard top: ~w-~w~n', [WarnaMeja, JenisMeja]),
-    write('Urutan pemain: '), printList(Urutan), write('.'), nl, nl,
+    write('Urutan pemain: '), 
+    cetakUrutan(Urutan, ArahMain), write('.'), nl, nl,
     cetakInfoPemain(Urutan, 1).
+
+cetakUrutan(Urutan, normal):-
+    printList(Urutan).
+
+cetakUrutan(Urutan, reverse):-
+    myReverse(Urutan, UrutanBaru),
+    printList(UrutanBaru).
 
 cetakInfoPemain([], _).
 cetakInfoPemain([Pemain | SisaPemain], IndexPemain):-
@@ -15,3 +24,9 @@ cetakInfoPemain([Pemain | SisaPemain], IndexPemain):-
     NextIndex is IndexPemain + 1,
     cetakInfoPemain(SisaPemain, NextIndex).
 
+myReverse(ListLama, ListBaru):-
+    reverseHelper(ListLama, [], ListBaru).
+
+reverseHelper([], ListBaru, ListBaru).
+reverseHelper([H|T], ListSementara, ListBaru):-
+    reverseHelper(T, [H|ListSementara], ListBaru).
