1795329d32091c9003fa77d2a161bea24cb7957f
Naufal Hasbialhaq <naufal.hasbialhaq18@gmail.com>
2026-05-28 14:57:32 +0700

    refactoring : penyesuaian kode agar bisa sembunyikanKartu

--- src/tangkap.pl
+++ src/tangkap.pl
@@ -10,26 +10,30 @@ tarik_kartu_aman(Target, N) :-
     N1 is N - 1,
     tarik_kartu_aman(Target, N1).
 
-% --- SKENARIO 0: VALIDASI PEMAIN TIDAK DITEMUKAN ---
+% VALIDASI PEMAIN TIDAK DITEMUKAN 
 tangkap(Target) :-
     \+ player(Target, _),
     !,
-    format('Sistem: Pemain dengan nama ~w tidak ditemukan!~n', [Target]).
-% --- SKENARIO 1: TANGKAPAN BERHASIL (TARGET BERSALAH) ---
+    format('Pemain dengan nama ~w tidak ditemukan!~n', [Target]).
+    
+% TANGKAPAN BERHASIL
 tangkap(Target) :-
     currentPlayer(Penuduh),
     player(Target, ListKartuTarget),
-    getLength(ListKartuTarget, 1),
+    getLength(ListKartuTarget, JumlahTerlihat),
+    myFindall(Kartu, kartu_tersembunyi(Target, Kartu), ListTersembunyi),
+    getLength(ListTersembunyi, JumlahTersembunyi),
+    TotalKartu is JumlahTerlihat + JumlahTersembunyi,
+    TotalKartu =:= 1,
     \+ status_uni(Target),
     !,
     format('~w tertangkap basah memiliki 1 kartu tapi tidak menyerukan UNI!~n', [Target]),
     format('Hukuman: ~w ditarik 2 kartu penalti.~n', [Target]),
     tarik_kartu_aman(Target, 2).
-
-
-% --- SKENARIO 2: TANGKAPAN GAGAL (PENUDUH SALAH TUDUH / FITNAH) 
-tangkap(Target) :-
-    currentPlayer(Penuduh),
-    format('Tangkapan gagal! ~w tidak melanggar aturan (kartu > 1 atau sudah bilang UNI).~n', [Target]),
-    format('Hukuman: ~w (sebagai penuduh) ditarik 1 kartu penalti.~n', [Penuduh]),
-    tarik_kartu_aman(Penuduh, 1).
+    
+% TANGKAPAN GAGAL 
+    tangkap(Target) :-
+        currentPlayer(Penuduh),
+        format('Yahh fitnah luu, ~w soalnya tidak melanggar aturan (Total kartu > 1 atau sudah bilang UNI).~n', [Target]),
+        format('Lu dihukum, ambil 1 kartu penalti.~n', [Penuduh]),
+        tarik_kartu_aman(Penuduh, 1).
