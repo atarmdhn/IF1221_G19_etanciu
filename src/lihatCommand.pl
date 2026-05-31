5567e4973001d94c588b44b60291e25c5edfdae1
kururi <fakhriyan.rizki@gmail.com>
2026-05-31 14:50:55 +0700

    feat: lihatCommand untuk mode turnamen

--- src/lihatCommand.pl
+++ src/lihatCommand.pl
@@ -17,7 +17,16 @@ cetak(Jenis) :-
     Jenis \= wild_draw_four, 
     write('Aksi Utama Yang Tersedia :'), nl,
     write('1. ambilKartu'), nl,
-    write('2. mainkanKartu'), nl.
+    write('2. mainkanKartu(NomorUrut)'), nl.
+
+cetakPendukung :- 
+    gameMode(turnamen), !,
+    write('Aksi Pendukung Yang Tersedia :'), nl,
+    write('1. lihatCommand'), nl,
+    write('2. lihatKartu'), nl,
+    write('3. cekInfo'), nl,
+    write('4. sembunyikanKartu(NomorUrut)'), nl,
+    write('5. swapKartu(NomorUrut, NomorUrutTeman)').
 
 cetakPendukung :-
     write('Aksi Pendukung Yang Tersedia :'), nl,
